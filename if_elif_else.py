62f61f34db0c974d543ec42b6eda63b797ce85b6
shubham5547 <64001725+shubham5547@users.noreply.github.com>
2020-04-27 23:42:52 +0530

    new .py file added

---
+++ if_elif_else.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+if True:
+    print('its true')
+
+
+# In[6]:
+
+
+if 3>2:
+    print('its true')
+
+
+# In[9]:
+
+
+if 3<5:
+    print('its false')
+
+
+# In[17]:
+
+
+hungry = True
+if hungry:
+    print('FEED ME!')
+else:
+    print('i am not hungry')
+
+
+# In[26]:
+
+
+loc = 'mall'
+if loc == 'auto shop':
+    print('every things is fine!')
+elif loc == 'bank':
+    print('money is cool!')
+elif loc == 'store':
+    print('welcome to store!')
+else:
+    print('not well')
+
+
+# In[35]:
+
+
+name = 'sanu'
+
+if name == 'gulli':
+    print('had dinner!')
+elif name == 'khusi':
+    print('had lunch!')
+elif name == 'krishna':
+    print('make kitchen!')
+else:
+    print('have breakfast!')
+
+
+# In[ ]:
+
+
+
+
